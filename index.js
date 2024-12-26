8062ed8ca6ca9ad47b41f796296fe9e7bd7f38fa
Levitifox <artfdanilkin17@gmail.com>
2024-12-26 23:52:08 +0100

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require("electron/main");
+
+app.whenReady().then(() => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+
+    win.loadFile("index.html");
+});
+
+app.on("window-all-closed", () => {
+    app.quit();
+});
