b59e9fda1206fd6276bca3d198ee7128749e6645
Levitifox <artfdanilkin17@gmail.com>
2024-12-27 01:36:55 +0100

    Build using TS

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-const { app, BrowserWindow } = require("electron/main");
+import { app, BrowserWindow } from "electron";
+
+process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = "true";
 
 app.whenReady().then(() => {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1100,
+        height: 700,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
