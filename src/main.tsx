15022e7c8779b5c75da64ddb17be0033afb69615
Levitifox <artfdanilkin17@gmail.com>
2024-12-27 02:33:04 +0100

    Drag and drop

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,42 @@
+import { webUtils } from "electron";
 import { createRoot } from "react-dom/client";
 import * as fs from "node:fs";
 
 console.log(fs);
 
+function Main() {
+    return (
+        <>
+            <h1>Shiftify</h1>
+            <div className="main-pane">
+                <div className="main-pane_row">
+                    <div className="main-pane_row_label">Input</div>
+                    <input
+                        className="main-pane_row_input"
+                        type="text"
+                        onDragOver={event => {
+                            event.preventDefault();
+                        }}
+                        onDrop={event => {
+                            event.preventDefault();
+                            const files = event.dataTransfer.files;
+                            if (files.length == 1) {
+                                const file = files[0];
+                                console.log(webUtils.getPathForFile(file));
+                            }
+                        }}
+                    ></input>
+                </div>
+                <div className="main-pane_row">
+                    <div className="main-pane_row_label">Output</div>
+                    <input className="main-pane_row_input" type="text"></input>
+                </div>
+            </div>
+        </>
+    );
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const root = createRoot(document.getElementById("app")!);
-    root.render(<h1>Hello, world</h1>);
+    root.render(<Main></Main>);
 });
